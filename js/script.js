016dc8172d210fe20c1515b769d70b65a7a56ff8
jonatasjdp <jonatasjdp@gmail.com]>
2023-01-24 10:38:01 -0300

    Finalizado

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver (entries => {
+    console.log(entries)
+
+    Array.from(entries).forEach(entry => {
+        if (entry.intersectionRatio >= 0.5){
+            entry.target.classList.add('init-hidden-off')
+        }             
+    })
+    
+}, {
+    threshold: [0, .5, 1]
+})
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach( element =>{
+    observer.observe(element)
+})
